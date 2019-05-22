4e86e62a08aab48bd60f9d25a4b1394a634a5689
Dzianis Matsiushyn <dzmats@gmail.com>
2019-05-22 09:20:21 +0200

    Added code part 1

---
+++ code/js_lesson1_src/2_array/js/scripts.js
@@ -0,0 +1,19 @@
+window.onload = function(e){
+    
+    var a = ['a' , 'b', 'c'];
+    for(var i = 0; i < a.length; i++){
+        console.log(a[i]);
+    }
+
+    var b = {
+        russia: "Moscow",
+        usa: 'Vashington',
+        knr: 'Bejing'
+    };
+
+    
+    for( in b){
+        console.log(b[key])
+    }
+}
+
